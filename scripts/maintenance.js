2f08420571ae314cc9859a95f8b22692b70859e7
Ryan RK <rk_ryan@icloud.com>
2021-08-28 22:31:19 +0800

    Modified robot behavior in maintenance.js

--- scripts/maintenance.js
+++ scripts/maintenance.js
@@ -8,20 +8,19 @@ function builderOff() {
     const robotBody = document.getElementById('robot-body');
     const robotArm = document.getElementById('robot-arm');
     const speechBubble = document.getElementById('speech-bubble');
-    // console.log(`angry value is ${angryValue}`);
     builderOffTrigger.style.visibility = 'hidden';
     onOffToggle(false);
     setTimeout(() => { restartBuilder(robotMain, robotBody, robotArm, builderOffTrigger, speechBubble) }, 1500);
-    if (angryValue >= 8) {
+    if (angryValue >= 6) {
         showSmokeSparks();
     }
-    if (angryValue > 12) {
+    if (angryValue > 9) {
         builderExploding();
     }
 }
 
 function restartBuilder(robotMain, robotBody, robotArm, builderOffTrigger, speechBubble) {
-    console.log('restartBuilder clicked');
+    // console.log('restartBuilder clicked');
     if (angryValue <= 2) {
         robotMain.style.animation = "robot-normal-move 3s ease-in-out";
         robotArm.style.animation = "arm-normal-move 3s ease-in-out";
@@ -30,7 +29,7 @@ function restartBuilder(robotMain, robotBody, robotArm, builderOffTrigger, speec
         robotMain.style.animation = "robot-angry-move 3s ease-in-out";
         robotArm.style.animation = "arm-angry-move 3s ease-in-out";
         setTimeout(() => { onOffToggle(true), speechBubble.style.visibility = 'hidden' }, 1400);
-    } else if (angryValue <= 6) {
+    } else if (angryValue <= 5) {
         robotBody.src = "assets/img/robot-angry.svg";
         robotMain.style.animation = "robot-angry-move 1.5s ease-in-out";
         robotArm.style.animation = "arm-angry-move 1.5s ease-in-out";
@@ -70,12 +69,6 @@ function showSmokeSparks() {
     spark2.style.visibility = 'visible';
 }
 
-// function builderExploded() {
-//     const 
-
-//     transform: rotateZ(8deg);
-// }
-
 function onOffToggle(isOn) {
     console.log('onOffToggle clicked');
     let runningState = 'running';
@@ -109,7 +102,6 @@ function onOffToggle(isOn) {
 
 function builderExploding() {
     console.log('builderExploding clicked');
-    // const builderBg = document.getElementById('builder-bg');
     const builderBgExplode = document.getElementById('builder-bg-explode');
     const explodeTransition = document.getElementById('explode-transition');
     const builderExploded = document.getElementById('builder-exploded');
@@ -117,16 +109,10 @@ function builderExploding() {
     const docMain = document.querySelector('main');
     const fgTextH1 = document.getElementsByClassName('foreground-text')[0].querySelector('h1');
     const fgTextP = document.getElementsByClassName('foreground-text')[0].querySelector('p');
-    // builderBgExplode.style.visibility = 'visible';
     builderBgExplode.style.display = 'inline';
-    // builderBg.style.animation = 'builder-blink 3s ease-in-out';
-    // builderBg.style.filter = 'brightness(0%) saturate(100%) invert(85%) sepia(0%) saturate(73%) hue-rotate(165deg) brightness(100%) contrast(95%)';
     explodeTransition.style.visibility = 'visible';
     explodeTransition.style.animation = 'explode-blink 4s ease-in-out';
     explodeTransition.style.animationDelay = '3s';
     setTimeout(() => { docMain.style.animation = 'shake 750ms cubic-bezier(.36, .07, .19, .97) both' }, 3300);
     setTimeout(() => { fgTextH1.style.transform = 'rotateZ(8deg)', fgTextH1.style.position = 'absolute', fgTextP.style.transform = 'scaleY(0.5)', builderExploded.style.display = 'flex', webBuilder.style.display = 'none'; }, 4000);
-    // transform-origin: left;
-    // transform: rotateZ(8deg);
-    // transform: rotateX(60deg);
 }
