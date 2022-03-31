45debfbf1770eca4d15686299293fe9d2be96e34
Ryan RK <rk_ryan@icloud.com>
2022-03-31 21:00:50 +0800

    Modify biodata section and mask animation

--- scripts/main.js
+++ scripts/main.js
@@ -63,47 +63,90 @@ function navListHover(pageID) {
 // --- Functions dealing with mask animation in biodata section ---
 
 function populateMask() {
-    console.log('mask start populating');
-    const maskFragmentsClass = document.getElementsByClassName('mask-fragments');
-    const mask = document.getElementById('mask');
-    const maskText = document.getElementsByClassName('mask-text');
+    const maskTextFg = document.getElementById('mask-text-fg');
+    if (maskTextFg.style.opacity != 1) {
+        return;
+    }
+    maskTextFg.style.opacity = 0;
     const clickSign1 = document.getElementById('click-sign1');
-    clickSign1.style.display = "none";
+    clickSign1.style.opacity = 0;
+    const maskFragmentsClass = document.getElementsByClassName('mask-fragments');
     for (const maskFragment of maskFragmentsClass) {
         maskFragment.style.animationName = 'fade-in-animation';
         maskFragment.style.animationDuration = '10ms';
     }
-    setTimeout(() => {
-        maskText[1].style.display = 'block';
-        maskText[2].style.display = 'block';
+    setTimeout(() => { maskTextFg.style.display = "none", clickSign1.style.display = "none", maskExplode(false); }, 4000);
+    setTimeout(() => { maskExplode(true); }, 5400);
+}
+
+function maskExplode(isReverse) {
+    const masktextBg = document.getElementById('mask-text-bg');
+    if (!isReverse) {
+        masktextBg.style.display = 'block';;
+        setTimeout(() => { masktextBg.style.opacity = 1; }, 100);
         shatterMask(1);
-    }, 4000);
-    setTimeout(() => {
-        shatterMask(0),
-            mask.style.transform = 'scale(0.8)',
-            mask.style.filter = 'blur(0.5rem)',
-            mask.style.opacity = 0.6,
-            mask.style.zIndex = 1;
-    }, 5400);
+        // for (const maskChar of maskChars) {
+        //     maskChar.style.opacity = 1;
+        // }
+    } else {
+        shatterMask(0);
+        const maskChars = document.getElementsByClassName('mask-chars');
+        for (const maskChar of maskChars) {
+            maskChar.style.animation = "exploded-char-animation 1400ms cubic-bezier(.73, .06, .82, .38) forwards";
+        }
+        const mask = document.getElementById('mask');
+        mask.style.transform = 'scale(0.8)';
+        // mask.style.filter = 'blur(0.5rem)';
+        mask.style.opacity = 0.6;
+        mask.style.zIndex = 1;
+        setTimeout(() => {
+            masktextBg.style.backgroundColor = "#bdbdbd5e",
+                masktextBg.style.backdropFilter = "blur(8px)";
+        }, 1000);
+        // const maskContainer = document.getElementsByClassName("mask-container");
+        // maskContainer[0].addEventListener("scroll", maskScroll, false);
+    }
+}
+
+function explodeMaskText() {
+    document.getElementById('mask-text-bg').innerHTML = document.getElementById('mask-text-bg').textContent.replace(/./g, "<span class=\"mask-chars\">$&</span>");
+
+    let explodedChar = document.getElementsByClassName("mask-chars");
+    for (let i = 0; i < explodedChar.length; i++) {
+        let left = innerWidth * Math.random();
+        let top = viewHeight * Math.random();
+        if (Math.random() < 0.5) {
+            explodedChar[i].style.left = "-" + left + "px";
+        } else {
+            explodedChar[i].style.left = left + "px";
+        }
+        if (Math.random() < 0.5) {
+            explodedChar[i].style.top = "-" + top + "px";
+        } else {
+            explodedChar[i].style.top = top + "px";
+        }
+    }
 }
 
 // Shatter or unshatter the mask
 function shatterMask(isShatter) {
     const maskFragments = document.getElementsByClassName('mask-fragments');
+    const xDistanceScale = 0.5;
     for (let i = 0; i < maskFragments.length; i++) {
         fragmentOffsetX = isShatter * 8 * maskFragmentsOffset[i][0];
         fragmentOffsetY = isShatter * 10 * maskFragmentsOffset[i][1];
         if (isShatter === 0) {
-            maskFragments[i].style.transition = 'transform 1500ms cubic-bezier(.73, .06, .82, .38)';
+            maskFragments[i].style.transition = 'transform 1400ms cubic-bezier(.73, .06, .82, .38)';
         } else {
-            maskFragments[i].style.transition = 'transform 1500ms cubic-bezier(.38, .82, .06, .73)';
+            maskFragments[i].style.transition = 'transform 1400ms cubic-bezier(.38, .82, .06, .73)';
         }
-        const randomDeg = Math.random() * (90 + 90) - 90;
-        const randomAxis1 = Math.random() * 2 - 1;
-        const randomAxis2 = Math.random() * 2 - 1;
-        const randomAxis3 = Math.random() * 2 - 1;
+        // const randomDeg = Math.random() * (90 + 90) - 90;
+        // const randomAxis1 = Math.random() * 2 - 1;
+        // const randomAxis2 = Math.random() * 2 - 1;
+        // const randomAxis3 = Math.random() * 2 - 1;
         // maskFragments[i].style.transform = `translate(${fragmentOffsetX}%, ${fragmentOffsetY}%) rotate(${isShatter * randomDeg}deg)`;
-        maskFragments[i].style.transform = `translate(${fragmentOffsetX}%, ${fragmentOffsetY}%) rotate3d(${randomAxis1}, ${randomAxis2}, ${randomAxis3}, ${isShatter * randomDeg}deg)`;
+        maskFragments[i].style.transform = `translate(${xDistanceScale * fragmentOffsetX}%, ${fragmentOffsetY}%) rotate3d(${maskRotationAxes[i][0]}, ${maskRotationAxes[i][1]}, ${maskRotationAxes[i][2]}, ${isShatter ? 180 : 360}deg)`;
+        // maskFragments[i].style.transform = `rotate3d(${maskRotationAxes[i][0]}, ${maskRotationAxes[i][1]}, ${maskRotationAxes[i][2]}, ${isShatter ? 180 : 360}deg)`;
     }
 }
 
@@ -126,9 +169,11 @@ function calcMaskOffset() {
         if (fragment < 25) {
             maskFragmentsOffset.push([(fragmentMeanXCoor - 147.5), (fragmentMeanYCoor - 80)]);
             maskOverallXOffset.push(147.5 - fragmentMeanXCoor);
+            maskFragments[fragment].style.transformOrigin = `${50}% ${50}%`;
         } else {
             maskFragmentsOffset.push([(fragmentMeanXCoor), (fragmentMeanYCoor - 80)]);
             maskOverallXOffset.push(fragmentMeanXCoor);
+            maskFragments[fragment].style.transformOrigin = `${0}% ${50}%`;
         }
     }
     // Calculate and set the animation delay of each fragments by sorting with the distance from center
@@ -141,6 +186,21 @@ function calcMaskOffset() {
         maskFragments[xOffsetSortOrder[i]].style.animationDelay = `${(Math.log(i*0.25+0.5)-Math.log(0.5))*600}ms`;
     }
     return maskFragmentsOffset;
+}
+
+function calculateMaskRotationAxis() {
+    const maskFragments = document.getElementsByClassName('mask-fragments');
+    var randomAxes = [];
+    for (let i = 0; i < maskFragments.length; i++) {
+        const randomAxis1 = Math.random() * 2 - 1;
+        const randomAxis2 = Math.random() * 2 - 1;
+        const randomAxis3 = Math.random() * 2 - 1;
+        // const randomAxis1 = 0;
+        // const randomAxis2 = 1;
+        // const randomAxis3 = 0;
+        randomAxes.push([randomAxis1, randomAxis2, randomAxis3]);
+    }
+    return randomAxes;
 }
 
 
@@ -376,9 +436,11 @@ function scrollUpSignScroll() {
 }
 
 function noticeContainerScroll() {
+    const noticeContainer = document.getElementsByClassName('notice-container');
     const noticeTexts = document.getElementsByClassName('notice-text');
     const noticeBorders = document.getElementsByClassName('notice-border');
     if (noticeTexts[0].getBoundingClientRect().top <= viewHeight * 6 / 10) {
+        noticeContainer[0].style.opacity = 1;
         for (noticeText of noticeTexts) {
             noticeText.style.opacity = 1;
         }
@@ -402,11 +464,18 @@ function biodataScroll() {
     // const mask = document.getElementById('mask');
     // const maskText = document.getElementsByClassName('mask-text');
     const maskTextContainer = document.getElementById('mask-text-container');
+    const maskTextFg = document.getElementById('mask-text-fg');
+    const biotextContainer = document.getElementsByClassName('biotext-container');
     // const maskFragmentsClass = document.getElementsByClassName('mask-fragments');
     const clickSignInner = document.getElementById('click-inner1');
     const clickSignOuter = document.getElementById('click-outer1');
-    if ((maskTextContainer.getBoundingClientRect().top + maskTextContainer.getBoundingClientRect().bottom) / 2 <= (viewHeight * 6 / 10)) {
+    if (biotextContainer[0].getBoundingClientRect().top <= (viewHeight * 6 / 10)) {
+        biotextContainer[0].style.transform = "scaleY(1)";
+    }
+    if ((maskTextContainer.getBoundingClientRect().top + maskTextContainer.getBoundingClientRect().bottom) / 2 <= (viewHeight * 8 / 10)) {
+        maskTextFg.style.opacity = 1;
         // maskText[0].style.opacity = 1;
+        // biotextPanel[1].style.transform = "scaleY(1)";
         clickSignInner.style.animation = 'click-inner-animation 2s ease-in-out infinite';
         clickSignOuter.style.animation = 'click-outer-animation 2s ease-in-out infinite';
         // setTimeout(() => {
@@ -427,6 +496,28 @@ function biodataScroll() {
         //         mask.style.zIndex = 1;
         // }, 5400);
         window.removeEventListener("scroll", biodataScroll);
+    }
+}
+
+function artProgrammingScroll() {
+    const masktextBg = document.getElementById('mask-text-bg');
+    if (masktextBg.style.opacity == 0) {
+        return;
+    }
+    if (masktextBg.getBoundingClientRect().top <= (viewHeight * 4 / 10)) {
+        // const irrelevantChars = document.getElementsByClassName('irrelevant-span');
+        // for (const irrelevantChar of irrelevantChars) {
+        //     irrelevantChar.style.opacity = 0;
+        // }
+        const biodataPanelContainer = document.getElementsByClassName('biodata-panel-container');
+        // biodataPanelContainer[0].style.display = "flex";
+        const biodataPanels = document.getElementsByClassName('biodata-panel');
+        for (const biodataPanel of biodataPanels) {
+            setTimeout(() => {
+                biodataPanel.style.transform = "scale(1,1)";
+            }, 500);
+        }
+        window.removeEventListener("scroll", artProgrammingScroll);
     }
 }
 
@@ -497,24 +588,27 @@ function footerLineScroll() {
     }
 }
 
-const maskFragmentsOffset = calcMaskOffset();
 const viewHeight = document.documentElement.clientHeight;
 const viewWidth = document.documentElement.clientWidth;
+const maskFragmentsOffset = calcMaskOffset();
+const maskRotationAxes = calculateMaskRotationAxis();
 const generalDirec = document.getElementById('general-direc');
 const skillsCategories = document.getElementsByClassName('skills-category');
-const dotsContainerWidth = createDots();
-transformDots(0);
-createSoftSkillsSwipe();
+// const dotsContainerWidth = createDots();
+// transformDots(0);
+explodeMaskText();
+// createSoftSkillsSwipe();
 navListHover('home');
 window.addEventListener("scroll", scrollUpSignScroll, false);
 window.addEventListener("scroll", headingScroll, false);
 window.addEventListener("scroll", lineDecorScroll, false);
 window.addEventListener("scroll", noticeContainerScroll, false);
 window.addEventListener("scroll", biodataScroll, false);
+window.addEventListener("scroll", artProgrammingScroll, false);
 // window.addEventListener("scroll", uiuxScroll, false);
 window.addEventListener("scroll", projectIconScroll, false);
 window.addEventListener("scroll", skillDirecScroll, false);
-window.addEventListener("scroll", softSkillsScroll, false);
+// window.addEventListener("scroll", softSkillsScroll, false);
 window.addEventListener("scroll", contactScroll, false);
 window.addEventListener("scroll", footerLineScroll, false);
 
@@ -535,8 +629,8 @@ terminalBackButton.addEventListener("click", function() { skillsCategoryClicked(
 
 
 // Horizontal scrolling for dots bg in softskills section
-const dotBG = document.querySelector(".scroll-wrapper");
-dotBG.addEventListener("scroll", dotScroll, false);
+// const dotBG = document.querySelector(".scroll-wrapper");
+// dotBG.addEventListener("scroll", dotScroll, false);
 
 
 
